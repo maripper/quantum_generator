aa0a1a042d64b41318cd8523cc60b18853f86dab
Marina <maripper2@gmail.com>
2022-05-29 21:27:26 -0300

    F6

--- base/views.py
+++ base/views.py
@@ -13,17 +13,17 @@ def quantum_computer(request):
 @csrf_exempt
 def start(requests):
     n=0
-    sender= req.post('https://qubit-operators.herokuapp.com/measurements/restart/',data={ }) 
+    sender= req.post('https://qubit-operators.herokuapp.com/qubit_operators/restart/',data={ }) 
     while(n<32):
         Q = [0, 1]
         print(random.choice(Q))
         Q = random.choice(Q)
-        sender= req.post('https://qubit-operators.herokuapp.com/measurements/receiver/',data={'bit':Q})
-        receiver= req.post('https://qubit-operators.herokuapp.com/measurements/sender/',data={'bit':Q})
+        sender= req.post('https://qubit-operators.herokuapp.com/qubit_operators/receiver/',data={'bit':Q})
+        receiver= req.post('https://qubit-operators.herokuapp.com/qubit_operators/sender/',data={'bit':Q})
         print(sender,receiver)
         n = n+1 
         print(Q,n)
-    dict = req.post('https://qubit-operators.herokuapp.com/measurements/compare/',data={'size':32})
+    dict = req.post('https://qubit-operators.herokuapp.com/qubit_operators/compare/',data={'size':32})
     # print(type(dict))  
 
     return JsonResponse(dict.json())
